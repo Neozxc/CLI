13c73d82db3bfcdf97e74c180b5e231feacee0bb
Lapinas <mrlapinas99@gmail.com>
2022-01-31 11:56:04 +0000

    Mornings challenge completion

--- src/app.js
+++ src/app.js
@@ -1,16 +1,53 @@
-const { Movie, movieArray } = require("./utils/index");
+// ===== MORNINGS EXAMPLE =====
 
-const app = (args) => {
-    switch (args[2]) {
-        case "add":
-            const movie = new Movie(args[3], args[4]);
-            movie.add();
-            console.log(movieArray);
-        break;
+// const { Movie, movieArray } = require("./utils/index");
+
+// const app = (args) => {
+//     switch (args[2]) {
+//         case "add":
+//             const movie = new Movie(args[3], args[4]);
+//             movie.add();
+//             console.log(movieArray);
+//         break;
             
-        default: 
-            console.log("Theres no 'add' command!");
-    }
+//         default: 
+//             console.log("Theres no 'add' command!");
+//     }
+// };
+
+// app(process.argv);
+
+// ===== MORNINGS EXAMPLE =====
+
+
+// ===== TASK =====
+
+// Define & Require
+const { Games, gamesArray } = require("./utils/index");
+
+const run = (x) => {
+    // Switch Statement
+    switch(x[2]) {
+        case "add":
+            // Args:
+            // x[3] = game
+            // x[4] = type
+            // x[5] = year
+            // x[6] = age 
+            const games = new Games(x[3], x[4], x[5], x[6]);
+            // We add to the array
+            games.add();
+            // We log it
+            console.log(gamesArray);
+
+    // Break;
+    break;
+
+    // Default:
+    default:
+        console.log("There was no command ran!");
+    };
 };
 
-app(process.argv);
+// Run run
+run(process.argv);
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,30 +1,63 @@
-const sayHello = () => {
-    console.log("Hello World");
-};
+// ===== MORNINGS EXAMPLE =====
 
-sayHello();
+// const sayHello = () => {
+//     console.log("Hello World");
+// };
 
-
-// Built into Node, type of application and what application it is running.
-console.log(process.argv);
-
-const movieArray = [];
-
-class Movie {
-
-    // Template to pass some data later on
-    constructor(title, actor = "N/A") {
-        this.title = title;
-        this.actor = actor;
-    }
+// sayHello();
 
 
+// // Built into Node, type of application and what application it is running.
+// console.log(process.argv);
+
+// const movieArray = [];
+
+// class Movie {
+
+//     // Template to pass some data later on
+//     constructor(title, actor = "N/A") {
+//         this.title = title;
+//         this.actor = actor;
+//     }
+
+
+//     add() {
+//         movieArray.push(this);
+//     }
+// };
+
+// module.exports = {
+//     Movie,
+//     movieArray
+// };
+
+// ===== MORNINGS EXAMPLE =====
+
+
+// ===== TASK =====
+
+
+// Define empty array;
+const gamesArray = [];
+
+// Make class
+class Games {
+    // Make constructor
+    constructor(game, type, year, age) {
+        this.game = game;
+        this.type = type;
+        this.year = year;
+        this.age = age;
+    };
+
+    // Function
     add() {
-        movieArray.push(this);
-    }
+        gamesArray.push(this);
+    };
 };
 
+// Export it
 module.exports = {
-    Movie,
-    movieArray
-};
+    Games,
+    gamesArray
+}
