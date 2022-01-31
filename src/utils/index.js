09166f8d3a3e933c6a916b0b94c0ccd91edeeed1
Lapinas <mrlapinas99@gmail.com>
2022-01-31 10:51:28 +0000

    Added a Class

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,3 +7,24 @@ sayHello();
 
 // Built into Node, type of application and what application it is running.
 console.log(process.argv);
+
+const movieArray = [];
+
+class Movie {
+
+    // Template to pass some data later on
+    constructor(title, actor = "N/A") {
+        this.title = title;
+        this.actor = actor;
+    }
+
+
+    add() {
+        movieArray.push(this);
+    }
+};
+
+module.expors = {
+    Movie,
+    movieArray
+};
